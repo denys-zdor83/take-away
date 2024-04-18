97cffb30ff2b1d84acd95d0f318398a0610a9a27
Denys <denys1984.25@gmail.com>
2024-04-17 23:45:17 -0600

    Categories: Created list of categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
+
 function Categories() {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+  const [activeCategory, setActiveCategory] = useState(0)
+
+  const onCategoryClick = (index) => {
+    setActiveCategory(index)
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((categoryName, index) => {
+          return (
+            <li 
+              onClick={() => onCategoryClick(index)} 
+              className={activeCategory === index ? 'active' : ''} 
+              key={categoryName}
+            >
+              {categoryName}
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
