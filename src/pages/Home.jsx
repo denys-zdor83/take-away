76c05ed759a8b090e98c377c3ef0365b24666819
Denys <denys1984.25@gmail.com>
2024-05-03 23:05:05 -0600

    Storage: Replaced part of stage of Categories and Sort components to the Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,35 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 function Home() {
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [sortType, setSortType] = useState({
+    name: 'popularity (desc)',
+    sortProperty: '-rating',
+  });
 
   useEffect(() => {
-    fetch('https://c988e3cd7ecb047d.mokky.dev/pizzas')
+    const category = categoryId > 0 ? `category=${categoryId}` : ''
+    setIsLoading(true);
+    fetch(`https://c988e3cd7ecb047d.mokky.dev/pizzas?${ category }&sortBy=${ sortType.sortProperty }`)
       .then((response) => response.json())
       .then((json) => {
-        setPizzas(json)
-        setIsLoading(false)
+        setPizzas(json);
+        setIsLoading(false);
       });
-      window.scrollTo(0, 0);
-  }, []);
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories 
+          value={categoryId} 
+          onChangeCategory={setCategoryId} 
+        />
+        <Sort 
+          value={sortType} 
+          onChangeSort={setSortType} 
+        />
       </div>
       <h2 className="content__title">All pizzas</h2>
       <div className="content__items">
