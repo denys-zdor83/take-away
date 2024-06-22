98ae634eae0749bc2b38361078fb5906e9e9cae8
Denys <denys1984.25@gmail.com>
2024-06-21 23:45:01 -0600

    Refactoring: Added pizza counter

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -4,19 +4,19 @@ import { addItem } from '../../redux/slices/cartSlice';
 
 function PizzaBlock({ id, title, price, imageUrl: image, sizes, types }) {
   const doughTypes = ['thin', 'traditional'];
-
+  const cartItem = useSelector((state) => state.cart.items.find((obj) => obj.id === id) );
+  const addedCount = cartItem ? cartItem.count : 0;
   const [activeType, setActiveType] = useState(0);
   const [activeSize, setActiveSize] = useState(0);
   const dispatch = useDispatch();
 
   const onClickAdd = () => {
-    
     const item = {
       id,
       title,
       price,
       imageUrl: image,
-      type: activeType,
+      type: doughTypes[activeType],
       size: activeSize,
     }
     dispatch(addItem(item));
@@ -61,7 +61,7 @@ function PizzaBlock({ id, title, price, imageUrl: image, sizes, types }) {
               <path d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z" fill="white"></path>
             </svg>
             <span>Add</span>
-            <i>{0}</i>
+            { addedCount > 0 && <i>{addedCount}</i> }
           </button>
         </div>
       </div> 
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,8 +10,18 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addItem(state, action) {
-      state.items.push(action.payload)
-      state.totalPrice = state.items.reduce((sum, obj) => obj.price + sum, 0)
+      const findItem = state.items.find(obj => obj.id === action.payload.id)
+      
+      if (findItem) {
+        findItem.count++
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1
+        })
+      }
+
+      state.totalPrice = state.items.reduce((sum, obj) => obj.price * obj.count + sum, 0)
     },
     removeItem(state, action) {
       state.items.filter(obj => obj.id !== action.payload)
